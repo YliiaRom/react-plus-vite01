b2627cb620f6a70b114d1dc92a7f547437488808
Romanenko Yliia(mac) <romany0507306677@gmail.com>
2025-06-09 17:12:12 +0300

    modif vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-task02/",
+  base: "/react-plus-vite01/",
   plugins: [react()],
 });
